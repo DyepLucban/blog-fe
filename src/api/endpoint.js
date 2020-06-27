fe5b440e446bf42fb54f76f30e5747b32c3bb701
Jeffrey Lucban <lucbanjep@gmail.com>
2020-06-27 15:21:02 +0800

    api

--- src/api/endpoint.js
+++ src/api/endpoint.js
@@ -1,12 +1,12 @@
 export default {
 	blog: {
-        get_all_blogs: 'http://heroku-blog-api.herokuapp.com/api/blog',
-        get_specific_blog: 'http://heroku-blog-api.herokuapp.com/api/blog',
-        create_blog: 'http://heroku-blog-api.herokuapp.com/api/blog',
-        update_blog: 'http://heroku-blog-api.herokuapp.com/api/blog',
-        delete_blog: 'http://heroku-blog-api.herokuapp.com/api/blog',
-        deleted_blog: 'http://heroku-blog-api.herokuapp.com/api/deleted-blog',
-        restore_deleted: 'http://heroku-blog-api.herokuapp.com/api/restore-deleted',
-        search_blog: 'http://heroku-blog-api.herokuapp.com/api/search-blog',
+        get_all_blogs: 'https://heroku-blog-api.herokuapp.com/api/blog',
+        get_specific_blog: 'https://heroku-blog-api.herokuapp.com/api/blog',
+        create_blog: 'https://heroku-blog-api.herokuapp.com/api/blog',
+        update_blog: 'https://heroku-blog-api.herokuapp.com/api/blog',
+        delete_blog: 'https://heroku-blog-api.herokuapp.com/api/blog',
+        deleted_blog: 'https://heroku-blog-api.herokuapp.com/api/deleted-blog',
+        restore_deleted: 'https://heroku-blog-api.herokuapp.com/api/restore-deleted',
+        search_blog: 'https://heroku-blog-api.herokuapp.com/api/search-blog',
     },
 }
