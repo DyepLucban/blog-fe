c1ee74d8f14d805874c0e353181928c23635974c
Jeffrey Lucban <lucbanjep@gmail.com>
2020-06-27 14:28:37 +0800

    update endpoint for heroku

--- src/api/endpoint.js
+++ src/api/endpoint.js
@@ -1,12 +1,12 @@
 export default {
 	blog: {
-        get_all_blogs: 'http://127.0.0.1:8000/api/blog',
-        get_specific_blog: 'http://127.0.0.1:8000/api/blog',
-        create_blog: 'http://127.0.0.1:8000/api/blog',
-        update_blog: 'http://127.0.0.1:8000/api/blog',
-        delete_blog: 'http://127.0.0.1:8000/api/blog',
-        deleted_blog: 'http://127.0.0.1:8000/api/deleted-blog',
-        restore_deleted: 'http://127.0.0.1:8000/api/restore-deleted',
-        search_blog: 'http://127.0.0.1:8000/api/search-blog',
+        get_all_blogs: 'http://heroku-blog-api.herokuapp.com/api/blog',
+        get_specific_blog: 'http://heroku-blog-api.herokuapp.com/api/blog',
+        create_blog: 'http://heroku-blog-api.herokuapp.com/api/blog',
+        update_blog: 'http://heroku-blog-api.herokuapp.com/api/blog',
+        delete_blog: 'http://heroku-blog-api.herokuapp.com/api/blog',
+        deleted_blog: 'http://heroku-blog-api.herokuapp.com/api/deleted-blog',
+        restore_deleted: 'http://heroku-blog-api.herokuapp.com/api/restore-deleted',
+        search_blog: 'http://heroku-blog-api.herokuapp.com/api/search-blog',
     },
 }
